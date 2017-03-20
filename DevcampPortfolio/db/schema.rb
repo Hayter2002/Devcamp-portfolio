77f2e91b2f6a9542a2ba94822abc1f00b73a7b4b
Mac <mac@Macs-MacBook-Pro.local>
2017-03-19 21:31:58 -0600

    Integrated skills via model generator

--- DevcampPortfolio/db/schema.rb
+++ DevcampPortfolio/db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312025734) do
+ActiveRecord::Schema.define(version: 20170319232331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
-    t.text     "body"
+    t.text     "bdy"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
