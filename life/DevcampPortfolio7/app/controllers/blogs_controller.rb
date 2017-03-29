f9de26060a29911c315e983b5ad2db62567122ee
Mac <mac@Macs-MacBook-Pro.local>
2017-03-29 11:17:41 -0600

    Integrated new and create actions for portfolios

--- life/DevcampPortfolio7/app/controllers/blogs_controller.rb
+++ life/DevcampPortfolio7/app/controllers/blogs_controller.rb
@@ -29,10 +29,10 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        
       end
     end
   end
@@ -57,7 +57,7 @@ class BlogsController < ApplicationController
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
-      format.json { head :no_content }
+      
     end
   end
 
